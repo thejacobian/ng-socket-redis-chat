2b46fec5dacb9723c447ab4c645261c686ec5cbd
Jake Shelton <jake.s.shelton@gmail.com>
2019-07-29 15:48:52 -0600

    add routes and template html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable quote-props */
 // Requires and Setup
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -43,7 +45,6 @@ fs.readFile('creds.json', 'utf-8', (err, data) => {
 
   // Redis Client Ready
   client.once('ready', () => {
-
     // Flush Redis DB
     // client.flushdb();
 
@@ -60,5 +61,71 @@ fs.readFile('creds.json', 'utf-8', (err, data) => {
         chat_messages = JSON.parse(reply);
       }
     });
+  });
+});
+
+// API - Join Chat
+app.post('/join', (req, res) => {
+  const username = req.body.username;
+  if (chatters.indexOf(username) === -1) {
+    chatters.push(username);
+    client.set('chat_users', JSON.stringify(chatters));
+    res.send({
+      'chatters': chatters,
+      'status': 'OK'
+    });
+  } else {
+    res.send({
+      'status': 'FAILED'
+    });
+  }
+});
+
+// API - Leave Chat
+app.post('/leave', (req, res) => {
+  const username = req.body.username;
+  chatters.splice(chatters.indexOf(username), 1);
+  client.set('chat_users', JSON.stringify(chatters));
+  res.send({
+    'status': 'OK'
+  });
+});
+
+// API - Send + Store Message
+app.post('/send_message', (req, res) => {
+  const username = req.body.username;
+  const message = req.body.message;
+  chat_messages.push({
+    'sender': username,
+    'message': message,
+  });
+  client.set('chat_app_messages', JSON.stringify(chat_messages));
+  res.send({
+    'status': 'OK',
+  });
+});
+
+// API - Get Messages
+app.get('/get_messages', (req, res) => {
+  res.send(chat_messages);
+});
+
+// API - Get Chatters
+app.get('/get_chatters', (req, res) => {
+  res.send(chatters);
+});
+
+// Socket Connection
+// UI Stuff
+io.on('connection', (socket) => {
+
+  // Fire 'send' event for updating Message list in UI
+  socket.on('message', (data) => {
+    io.emit('send', data);
+  });
+
+  // Fire 'count_chatters' for updating Chatter Count in UI
+  socket.on('update_chatter_count', (data) => {
+    io.emit('count_chatters', data);
   });
 });
